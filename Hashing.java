47a8da12ac724f60b631fa8a30235f12cc60c5eb
Nikhilesh1119 <nikhileshchouhan20685@acropolis.in>
2023-06-28 18:34:59 +0530

    Hashing

--- Hashing.java
+++ Hashing.java
@@ -4,11 +4,12 @@ public class Hashing {
     public static void main(String[] args) {
         int a[] = { 1, 8, 4, 5, 9, 3 };
         int b[] = { 9, 4, 2, 8, 1, 7 };
+        int c[] = { 1, 2, 2, 1, 3, 1, 1, 3 };
         // union(a, b);
         // intersection(a, b);
         // subArrayOfSum(a, 9);
         // subArrayOfSum(a, 6);
-        
+        // distintElement(c, 4);
     }
 
     static void union(int a[], int b[]) {
@@ -64,6 +65,23 @@ public class Hashing {
             System.out.println("not found");
         } else {
             System.out.println("found");
+        }
+    }
+
+    static void distintElement(int a[], int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < k; i++) {
+            map.put(a[i], map.getOrDefault(a[i], 0) + 1);
+        }
+        System.out.println(map.size());
+        for (int i = k; i < a.length; i++) {
+            if (map.get(a[i - k]) == 1) {
+                map.remove(a[i - k]);
+            } else {
+                map.put(a[i - k], map.getOrDefault(a[i - k], 0) - 1);
+            }
+            map.put(a[i], map.getOrDefault(a[i], 0) + 1);
+            System.out.println(map.size());
         }
     }
 }
